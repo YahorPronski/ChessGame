0f30a145dbedbad846e6ba4bfbdcdeddeadb10b6
GavrilikArt <gavrilik.art@gmail.com>
2021-03-21 15:51:13 +0300

    Nothin

--- kursovaya_final/OnClickHandler.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Data.h"
-
-#include <SFML/System/Vector2.hpp>
-#include <map>
-
-std::map<short, short> onFirstClick(Square** field, bool isLeftField, sf::Vector2i mousePosition);
