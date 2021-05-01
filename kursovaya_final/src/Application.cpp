b59a48e9d165140240c2e6d3bfdcf2a6b9cd0ea1
GavrilikArt <gavrilik.art@gmail.com>
2021-05-01 20:58:54 +0300

    Finalized timer, added restart.

--- kursovaya_final/src/Application.cpp
+++ kursovaya_final/src/Application.cpp
@@ -20,7 +20,11 @@
 
 std::string timeFormat(int time) {
     std::stringstream ss;
-    ss << time/ 60 << ":" << time % 60;
+    if (time % 60 < 10 && time % 60 != 0) {
+        ss << time / 60 << ":" << 0 << time % 60;
+    } else {
+        ss << time / 60 << ":" << time % 60;
+    }
     if (time % 60 == 0) {
         ss << 0;
     }
@@ -40,6 +44,8 @@ int main()
     bool showMoves = false;
     bool gameStarted = false;
     bool isWhitesMove = true;
+    bool gameEnded = false;
+    bool whiteWon;
 
     Options options;
     TextureCoordinatesContainer container;
@@ -59,6 +65,9 @@ int main()
     if (!fieldBordersTexture.load()) return -1;
 
     sf::Font myFont;
+    sf::Text win;
+    Button restart = Button(squareSize * 10, squareSize * 11, squareSize * 2, squareSize / 2);
+    sf::Text restartText;
     if (!myFont.loadFromFile(font)) {
         throw "Flaj";
     }
@@ -89,115 +98,141 @@ int main()
         }
         if (gameStarted) {
             if (clock.getElapsedTime().asSeconds() > 1) {
-                if (isWhitesMove) {
-                    whiteCountdown--;
-                    whiteTimer.setString(timeFormat(whiteCountdown));
-                    clock.restart();
+                if (whiteCountdown <= 0 || blackCountdown <= 0) {
+                    gameEnded = true;
+                    whiteWon = whiteCountdown > 0;
                 } else {
-                    blackCountdown--;
-                    blackTimer.setString(timeFormat(blackCountdown));
-                    clock.restart();
+                    if (isWhitesMove) {
+                        whiteCountdown--;
+                        whiteTimer.setString(timeFormat(whiteCountdown));
+                        clock.restart();
+                    } else {
+                        blackCountdown--;
+                        blackTimer.setString(timeFormat(blackCountdown));
+                        clock.restart();
+                    }
                 }
             }
         }
 
-        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-
-            sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
-            //Button checking
-            if (!gameStarted) {
-                if (options.oneMinute.isSelected(mousePosition)) {
-                    whiteCountdown = blackCountdown = 60;
-                    blackTimer.setString(timeFormat(blackCountdown));
-                    whiteTimer.setString(timeFormat(whiteCountdown));
-                } else if (options.twoMinutes.isSelected(mousePosition)) {
-                    whiteCountdown = blackCountdown = 120;
-                    blackTimer.setString(timeFormat(blackCountdown));
-                    whiteTimer.setString(timeFormat(whiteCountdown));
-                } else if (options.threeMinutes.isSelected(mousePosition)) {
-                    whiteCountdown = blackCountdown = 180;
-                    blackTimer.setString(timeFormat(blackCountdown));
-                    whiteTimer.setString(timeFormat(whiteCountdown));
-                } else if (options.fiveMinutes.isSelected(mousePosition)) {
-                    whiteCountdown = blackCountdown = 300;
-                    blackTimer.setString(timeFormat(blackCountdown));
-                    whiteTimer.setString(timeFormat(whiteCountdown));
-                } else if (options.tenMinutes.isSelected(mousePosition)) {
-                    whiteCountdown = blackCountdown = 600;
-                    blackTimer.setString(timeFormat(blackCountdown));
-                    whiteTimer.setString(timeFormat(whiteCountdown));
-                } else if (options.fifteenMinutes.isSelected(mousePosition)) {
-                    whiteCountdown = blackCountdown = 900;
-                    blackTimer.setString(timeFormat(blackCountdown));
-                    whiteTimer.setString(timeFormat(whiteCountdown));
-                }
-            }
-            //
-            currentField = isLeftField ? leftField : rightField;
-
-            if (firstClickAccess) {
-                firstClickAccess = false;
-
-                if (isCorrectClick(isLeftField, mousePosition)) {
-                    fromSquare = findSquareByCoordinates(currentField, isLeftField, mousePosition);
-
-                    // Show moves if true
-                    if (fromSquare.pieceColor == (isLeftField ? WHITE : BLACK)) {
-
-                        currentMoves = findCurrentPossibleMoves(currentField, fromSquare);
-
-                        movesTexture.load(currentMoves, isLeftField);
-                        showMoves = true;
-
-                    } else { firstClickAccess = true; }
-                } else {firstClickAccess = true;}
-            } else {
-
-                // Make move if true
-                if (isCorrectMove(currentMoves, isLeftField, mousePosition)) {
-
-                    toSquare = findSquareByCoordinates(currentField, isLeftField, mousePosition);
-                    makeMove(leftField, rightField, isLeftField, fromSquare, toSquare);
-                    showMoves = false;
-
-                    chessTexture.load(leftField, rightField, container);
-
-                    // Change action if mate
-                    if (isCheckMate(isLeftField ? rightField : leftField, isLeftField ? BLACK : WHITE)) std::cout<<"Mate!!!" << std::endl;
-
-                    isLeftField = isLeftField ? false : true;
-                    firstClickAccess = true;
-                    //timer.stop
-                    gameStarted = true;
-                    isWhitesMove = !isWhitesMove;
-                }
-                else if (isCorrectClick(isLeftField, mousePosition)) {
-
-                    fromSquare = findSquareByCoordinates(currentField, isLeftField, mousePosition);
-
-                    // Show moves if true
-                    if (fromSquare.pieceColor == (isLeftField ? WHITE : BLACK)) {
-                        currentMoves = findCurrentPossibleMoves(currentField, fromSquare);
-
-                        movesTexture.load(currentMoves, isLeftField);
-                        showMoves = true;
-
+        if (!gameEnded) {
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+                sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+                //Button checking
+                if (!gameStarted) {
+                    if (options.oneMinute.isSelected(mousePosition)) {
+                        whiteCountdown = blackCountdown = 60;
+                        blackTimer.setString(timeFormat(blackCountdown));
+                        whiteTimer.setString(timeFormat(whiteCountdown));
+                    } else if (options.twoMinutes.isSelected(mousePosition)) {
+                        whiteCountdown = blackCountdown = 120;
+                        blackTimer.setString(timeFormat(blackCountdown));
+                        whiteTimer.setString(timeFormat(whiteCountdown));
+                    } else if (options.threeMinutes.isSelected(mousePosition)) {
+                        whiteCountdown = blackCountdown = 180;
+                        blackTimer.setString(timeFormat(blackCountdown));
+                        whiteTimer.setString(timeFormat(whiteCountdown));
+                    } else if (options.fiveMinutes.isSelected(mousePosition)) {
+                        whiteCountdown = blackCountdown = 300;
+                        blackTimer.setString(timeFormat(blackCountdown));
+                        whiteTimer.setString(timeFormat(whiteCountdown));
+                    } else if (options.tenMinutes.isSelected(mousePosition)) {
+                        whiteCountdown = blackCountdown = 600;
+                        blackTimer.setString(timeFormat(blackCountdown));
+                        whiteTimer.setString(timeFormat(whiteCountdown));
+                    } else if (options.fifteenMinutes.isSelected(mousePosition)) {
+                        whiteCountdown = blackCountdown = 900;
+                        blackTimer.setString(timeFormat(blackCountdown));
+                        whiteTimer.setString(timeFormat(whiteCountdown));
                     }
-                    // Hide moves
+                }
+                //
+                currentField = isLeftField ? leftField : rightField;
+
+                if (firstClickAccess) {
+                    firstClickAccess = false;
+
+                    if (isCorrectClick(isLeftField, mousePosition)) {
+                        fromSquare = findSquareByCoordinates(currentField, isLeftField, mousePosition);
+
+                        // Show moves if true
+                        if (fromSquare.pieceColor == (isLeftField ? WHITE : BLACK)) {
+
+                            currentMoves = findCurrentPossibleMoves(currentField, fromSquare);
+
+                            movesTexture.load(currentMoves, isLeftField);
+                            showMoves = true;
+
+                        } else { firstClickAccess = true; }
+                    } else {firstClickAccess = true;}
+                } else {
+
+                    // Make move if true
+                    if (isCorrectMove(currentMoves, isLeftField, mousePosition)) {
+
+                        toSquare = findSquareByCoordinates(currentField, isLeftField, mousePosition);
+                        makeMove(leftField, rightField, isLeftField, fromSquare, toSquare);
+                        showMoves = false;
+
+                        chessTexture.load(leftField, rightField, container);
+
+                        // Change action if mate
+                        if (isCheckMate(isLeftField ? rightField : leftField, isLeftField ? BLACK : WHITE)) {
+                            if (isWhitesMove) {
+                                whiteWon = true;
+                            }
+                            gameEnded = true;
+                        }
+
+                        isLeftField = isLeftField ? false : true;
+                        firstClickAccess = true;
+                        //timer.stop
+                        gameStarted = true;
+                        isWhitesMove = !isWhitesMove;
+                    }
+                    else if (isCorrectClick(isLeftField, mousePosition)) {
+
+                        fromSquare = findSquareByCoordinates(currentField, isLeftField, mousePosition);
+
+                        // Show moves if true
+                        if (fromSquare.pieceColor == (isLeftField ? WHITE : BLACK)) {
+                            currentMoves = findCurrentPossibleMoves(currentField, fromSquare);
+
+                            movesTexture.load(currentMoves, isLeftField);
+                            showMoves = true;
+
+                        }
+                            // Hide moves
+                        else {
+                            showMoves = false;
+                            firstClickAccess = true;
+                        }
+                    }
+                        // Hide moves
                     else {
                         showMoves = false;
                         firstClickAccess = true;
                     }
                 }
-                // Hide moves
-                else {
-                    showMoves = false;
-                    firstClickAccess = true;
+
+            }
+        } else {
+            win.setFont(myFont);
+            win.setString(whiteWon ? "White won" : "Black won");
+            win.setScale(3.0, 3.0);
+            win.setPosition(squareSize * 10, squareSize * 10);
+            restartText.setFont(myFont);
+            restartText.setString("Restart");
+            restartText.setScale(2.0, 2.0);
+            restartText.setPosition(squareSize*10, squareSize * 11);
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+                sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+                if (restart.isSelected(mousePosition)) {
+                    main();
+                    return 0;
                 }
             }
-
         }
-
         // Draw
         window.clear();
         window.draw(chessTexture);
@@ -206,9 +241,13 @@ int main()
         window.draw(options);
         window.draw(whiteTimer);
         window.draw(blackTimer);
+        if (gameEnded)  {
+            window.draw(win);
+            window.draw(restart.displayButton());
+            window.draw(restartText);
+        }
         if (showMoves) window.draw(movesTexture);
         window.display();
-
         sf::sleep(sf::milliseconds(10));
     }
 
